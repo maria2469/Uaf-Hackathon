8db68f8a4922a5c088d87171ca64b378773d7145
maria2468 <tomarianoor@gmail.com>
2025-10-29 23:14:22 +0500

    sdsdsdasa

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# upgrade pip and install requirements
-python -m ensurepip --upgrade
-python -m pip install --upgrade pip
-python -m pip install -r backend/requirements.txt
-
-# start FastAPI
-uvicorn backend.main:app --host 0.0.0.0 --port 8000
